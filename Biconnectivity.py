4056c7609ced8e22e53ecbdb1a77a7d70b6f666a
David Eppstein <eppstein@ics.uci.edu>
2004-04-20 21:31:50 +0000

    Additional unit testing for biconnectivity.

--- Biconnectivity.py
+++ Biconnectivity.py
@@ -68,9 +68,30 @@ class BiconnectivityTest(unittest.TestCase):
         5: [0,1,3,4],
         6: [3,4],
     }
-    
-    def test_isbc(self):
+    G2 = {
+        0: [2,5],
+        1: [3,8],
+        2: [0,3,5],
+        3: [1,2,6,8],
+        4: [7],
+        5: [0,2],
+        6: [3,8],
+        7: [4],
+        8: [1,3,6],
+    }
+
+    def testIsBiconnected(self):
+        """G1 is biconnected but G2 is not."""
         assert isBiconnected(self.G1)
+        assert not isBiconnected(self.G2)
+        
+    def testBiconnectedComponents(self):
+        """G2 has four biconnected components."""
+        C = BiconnectedComponents(self.G2)
+        for comp in C:
+            comp.sort()
+        C.sort()
+        self.assertEqual(C,[[0,2,5],[1,3,6,8],[2,3],[4,7]])
 
 if __name__ == "__main__":
     unittest.main()   
