e875329fd1bdf59033c1937c7877d494d3ddb4fe
David Eppstein <eppstein@ics.uci.edu>
2004-05-17 17:39:40 +0000

    Minor tweaks and doc for Eratosthenes.

--- Eratosthenes.py
+++ Eratosthenes.py
@@ -3,11 +3,19 @@ Space-efficient version of sieve of Eratosthenes.
 D. Eppstein, May 2004.
 
 The main storage of the algorithm is a hash table D with sqrt(n)
-nonempty entries for a total of O(sqrt n) space.  The time for the
-algorithm, up to output n, is O(n) + sum_{prime p <= sqrt(n)} O(n/p)
-= O(n log log n).  The algorithm also makes a recursive call, but the
-recursion only generates primes up to sqrt n so its time and space is
-negligible compared to the outer call.
+nonempty entries for a total of O(sqrt n) space.
+
+At any point in the algorithm, each prime p occupies a cell with key at
+most 2n.  E.g. by Bertrand's postulate, there is another prime p'
+between n/p and 2n/p, and p' can not yet have been included because it
+is greater than sqrt n, so key pp' can not be used by any other prime;
+therefore p is placed at or before key pp'<2n.  Thus, the number of
+times p can have been moved from its initial placement at p^2 is < n/p.
+
+The time for the algorithm, up to output n, is O(n) + sum_{prime p <=
+sqrt(n)} O(n/p) = O(n log log n).  The algorithm also makes a recursive
+call, but the recursion only generates primes up to sqrt n so its time
+and space is negligible compared to the outer call.
 
 Thanks to Alex Martelli for the suggestion of keeping one prime
 per entry of D, rather than a list of all prime factors of D.
@@ -24,24 +32,24 @@ def primes():
     square = root*root
     
     # The main sieving loop.
-    # We use a hash table D such that D[n]=p for p a prime factor of n.
+    # We use a hash table D such that D[n]=2p for p a prime factor of n.
     # Each prime p up to sqrt(n) appears once as a value in D, and is
     # moved to successive odd multiples of p as the sieve progresses.
     D = {}
     n = 3
     while True:
         if n >= square:     # Time to include another square?
-            D[square] = root
+            D[square] = root+root
             root = roots.next()
             square = root*root
 
         if n not in D:      # Not witnessed, must be prime.
             yield n
         else:               # Move witness p to next free multiple.
-            q = n
             p = D[n]
+            q = n+p
             while q in D:
-                q += p+p
+                q += p
             del D[n]
             D[q] = p
         n += 2              # Move on to next odd number.
