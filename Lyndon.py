520c58d6fa9c69cbee4d7db80dd6f2f3a756e4d7
David Eppstein <eppstein@ics.uci.edu>
2011-10-28 01:04:54 +0000

    ChenFoxLyndon

--- Lyndon.py
+++ Lyndon.py
@@ -64,6 +64,33 @@ def CountLyndonWords(s,n):
             total += MoebiusFunction(n/i) * s**i
     return total//n
 
+def ChenFoxLyndonBreakpoints(s):
+    """Find starting positions of Chen-Fox-Lyndon decomposition of s.
+    The decomposition is a set of Lyndon words that start at 0 and
+    continue until the next position. 0 itself is not output, but
+    the final breakpoint at the end of s is. The argument s must be
+    of a type that can be indexed (e.g. a list, tuple, or string).
+    The algorithm follows Duval, J. Algorithms 1983, but uses 0-based
+    indexing rather than Duval's choice of 1-based indexing."""
+    k = 0
+    while k < len(s):
+        i,j = k,k+1
+        while j < len(s) and s[i] <= s[j]:
+            i = (s[i] == s[j]) and i+1 or k     # Python cond?yes:no syntax
+            j += 1
+        while k < i+1:
+            k += j-i
+            yield k
+
+def ChenFoxLyndon(s):
+    """Decompose s into Lyndon words according to the Chen-Fox-Lyndon theorem.
+    The arguments are the same as for ChenFoxLyndonBreakpoints but the
+    return values are subsequences of s rather than indices of breakpoints."""
+    old = 0
+    for k in ChenFoxLyndonBreakpoints(s):
+        yield s[old:k]
+        old = k
+
 # If run standalone, perform unit tests
 class LyndonTest(unittest.TestCase):    
     def testCount(self):
