9ddd9b7d0657a44771a376834e9fa4fa71ef4f57
David Eppstein <david.eppstein@gmail.com>
2013-03-29 17:37:30 -0700

    Involutions in Permutations.py

--- Permutations.py
+++ Permutations.py
@@ -117,6 +117,67 @@ def StirlingPermutations(n):
         perm[x],perm[x+2] = perm[x+2],perm[x]
         yield perm
 
+def InvolutionChanges(n):
+    """Generate change sequence for involutions on n items.
+    Uses a variation of the Steinhaus-Johnson-Trotter idea,
+    in which we first recurse for n-1, generating involutions
+    in which the last item is fixed, and then we the match
+    for the last item back and forth over a recursively
+    generated sequence for n-2."""
+    if n <= 1:
+        return
+    elif n == 2:
+        yield 0
+        return
+    elif n == 3:
+        for c in [0,1,0]:
+            yield c
+        return
+    for c in InvolutionChanges(n-1):
+        yield c
+    yield n-2
+    for i in range(n-4,-1,-1):
+        yield i
+    ic = InvolutionChanges(n-2)
+    try:
+        while True:
+            yield ic.next() + 1
+            for i in range(0,n-2):
+                yield i
+            yield ic.next()
+            for i in range(n-3,-1,-1):
+                yield i
+    except StopIteration:
+        yield n-4
+
+def Involutions(n):
+    """Generate involutions on n items.
+    The first involution is always the one in which all items
+    are mapped to themselves, and the last involution is the one
+    in which only the final two items are swapped.
+    Each two involutions differ by a change that either adds or
+    removes an adjacent pair of swapped items, moves a swap target
+    by one, or swaps two adjacent swap targets."""
+    p = range(n)
+    yield p
+    for c in InvolutionChanges(n):
+        if p[c] == c:
+            if p[c+1] == c+1:
+                p[c],p[c+1] = c+1,c     # add new pair
+            else:                       # move end of one pair
+                i = p[c+1]
+                p[c],p[c+1],p[i] = i,c+1,c
+        elif p[c+1] == c+1:             # again, move end of one pair
+            i = p[c]
+            p[c],p[c+1],p[i] = c,i,c+1
+        elif p[c] == c+1:
+            p[c],p[c+1] = c,c+1         # remove one pair
+        else:                           # swap ends of two pairs
+            x,y = p[c],p[c+1]
+            p[x],p[y] = c+1,c
+            p[c],p[c+1] = y,x
+        yield p
+
 # If run standalone, perform unit tests
 class PermutationTest(unittest.TestCase):    
     def testChanges(self):
@@ -156,6 +217,20 @@ class PermutationTest(unittest.TestCase):
         """If given a list as input, is it the first output?"""
         for L in ([1,3,5,7], list('zyx'), [], [[]], range(20)):
             self.assertEqual(L,SteinhausJohnsonTrotter(L).next())
+
+    def testInvolutions(self):
+        """Are these involutions and do we have the right number of them?"""
+        telephone = [1,1,2,4,10,26,76,232]
+        for n in range(len(telephone)):
+            count = 0
+            sorted = range(n)
+            invs = set()
+            for p in Involutions(n):
+                self.assertEqual([p[i] for i in p],sorted)
+                invs.add(tuple(p))
+                count += 1
+            self.assertEqual(len(invs),count)
+            self.assertEqual(len(invs),telephone[n])
 
 if __name__ == "__main__":
     unittest.main()
