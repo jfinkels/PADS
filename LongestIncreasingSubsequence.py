e494917273d8e36bd28f891d23a09705cbf8bfe1
David Eppstein <david.eppstein@gmail.com>
2014-05-11 21:33:37 -0700

    Update LongestIncreasingSubsequence for Python3 compatibility

--- LongestIncreasingSubsequence.py
+++ LongestIncreasingSubsequence.py
@@ -53,9 +53,11 @@ class LISTest(unittest.TestCase):
     def testLIS(self):
         self.assertEqual(LongestIncreasingSubsequence([]),[])
         self.assertEqual(LongestIncreasingSubsequence(range(10,0,-1)),[1])
-        self.assertEqual(LongestIncreasingSubsequence(range(10)),range(10))
+        self.assertEqual(LongestIncreasingSubsequence(range(10)),
+                                                      list(range(10)))
         self.assertEqual(LongestIncreasingSubsequence([3,1,4,1,5,9,2,6,5,3,5,8,9,7,9]),
                                                       [1,2,3,5,8,9])
 
 if __name__ == "__main__":
     unittest.main()   
+
