8553bed50ce00d22ca1b908e3cfa144c4e60f3d7
David Eppstein <david.eppstein@gmail.com>
2014-05-11 21:51:39 -0700

    Update Medium.py for Python3 compatibility

--- Medium.py
+++ Medium.py
@@ -229,7 +229,7 @@ def RoutingTable(M):
     recognize that the input is not a medium, we raise MediumError.
     """
     G = StateTransitionGraph(M)
-    current = initialState = iter(M).next()
+    current = initialState = next(iter(M))
 
     # find list of tokens that lead to the initial state
     activeTokens = set()
